52dc5b81fab8ff6e099e07b2b88fc405fa814071
Lenok <olena.andriushchenko@powercoders.org>
2023-11-13 15:17:17 +0100

    create html and swiper sdn

---
+++ assets/main.js
@@ -0,0 +1,14 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+  
+  
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    slidesPerView: 3,
+  });
