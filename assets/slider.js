f39448c9a8dc461a5ba78ffa7cb28126cec68602
Lenok <olena.andriushchenko@powercoders.org>
2023-11-13 20:00:42 +0100

    slider/form/first screen NO adaptive

--- assets/slider.js
+++ assets/slider.js
@@ -1,14 +1,52 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     // direction: 'vertical',
-    loop: true,
-  
-  
-  
+    // loop: true,
+    spaceBetween: 90,
     // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+    autoplay: {
+        delay: 5000,
+      },
     slidesPerView: 2,
+    breakpoints: {
+        320: {
+          slidesPerView: 1,
+        },
+        700: {
+          slidesPerView: 2,
+          spaceBetween: 90
+        }
+      }
+
   });
+  const mybutton = document.querySelector('.totop')
+  
+  window.onscroll = function () {
+    scrollFunction();
+  }; 
+
+
+  mybutton.addEventListener("click", backToTop);
+    
+    function backToTop() {
+     window.scrollTo({ top: 0, behavior: 'smooth' });
+
+    }
+
+
+    function scrollFunction() {
+        if (
+          document.body.scrollTop > 20 ||
+          document.documentElement.scrollTop > 20
+        ) {
+          mybutton.style.zindex = "3";
+          mybutton.style.opacity = "1";
+        } else {
+            mybutton.style.zindex = "-99";
+            mybutton.style.opacity = "0";
+        }
+      }
